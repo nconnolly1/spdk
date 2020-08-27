bde9c35c2b717d0edf69726cd465245762a56e5f
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2020-08-27 17:47:22 +0100

    Update cfg.sh

--- cfg.sh
+++ cfg.sh
@@ -9,7 +9,7 @@ LD=lld-link
 CCAR=lld-link
 CFLAGS='-I$(SPDK_ROOT_DIR)/include/windows -I$(SPDK_ROOT_DIR)/../CUnit/include -I$(SPDK_ROOT_DIR)/../wpdk/include -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS -D_CRT_DECLARE_NONSTDC_NAMES=0'
 CXXFLAGS='-I$(SPDK_ROOT_DIR)/include/windows -I$(SPDK_ROOT_DIR)/../CUnit/include -I$(SPDK_ROOT_DIR)/../wpdk/include -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS -D_CRT_DECLARE_NONSTDC_NAMES=0'
-LDFLAGS='-L$(SPDK_ROOT_DIR)/../CUnit/lib'
+LDFLAGS='-L$(SPDK_ROOT_DIR)/../CUnit/lib -L$(SPDK_ROOT_DIR)/../wpdk/lib'
 export CC CXX CFLAGS CXXFLAGS LD LDFLAGS CCAR
 
 ./configure --with-dpdk=C:/Users/Nick/Repos/dpdk/build --enable-debug --without-isal --without-vhost  --without-virtio
