4a5a675705dc60f0648975d81559a864f24aa06a
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2020-09-07 08:29:02 +0100

    Remove redundant defines

--- cfg.sh
+++ cfg.sh
@@ -7,8 +7,8 @@ CC=clang
 CXX=clang
 LD=lld-link
 CCAR=lld-link
-CFLAGS='-I$(SPDK_ROOT_DIR)/include/windows -I$(SPDK_ROOT_DIR)/../CUnit/include -I$(SPDK_ROOT_DIR)/../wpdk/include -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS -D_CRT_DECLARE_NONSTDC_NAMES=0'
-CXXFLAGS='-I$(SPDK_ROOT_DIR)/include/windows -I$(SPDK_ROOT_DIR)/../CUnit/include -I$(SPDK_ROOT_DIR)/../wpdk/include -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS -D_CRT_DECLARE_NONSTDC_NAMES=0'
+CFLAGS='-I$(SPDK_ROOT_DIR)/../CUnit/include -I$(SPDK_ROOT_DIR)/../wpdk/include'
+CXXFLAGS='-I$(SPDK_ROOT_DIR)/../CUnit/include -I$(SPDK_ROOT_DIR)/../wpdk/include'
 LDFLAGS='-L$(SPDK_ROOT_DIR)/../CUnit/lib -L$(SPDK_ROOT_DIR)/../wpdk/lib'
 export CC CXX CFLAGS CXXFLAGS LD LDFLAGS CCAR
 
